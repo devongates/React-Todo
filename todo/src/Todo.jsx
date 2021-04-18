09a995408c5883cce686ca82e6ebcde1b456f035
Devon Gates <devonjgates@gmail.com>
2021-04-17 21:28:50 -0500

    Initialized app :tada:

---
+++ todo/src/Todo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useState, useEffect, useReducer } from 'react'
+
+const Todo = () => {
+	return (
+		<div>
+			<h1>Todo</h1>
+		</div>
+	)
+}
+
+export default Todo
