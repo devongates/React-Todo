6b3a6bbd1e85c1ac7148891da38ee4f492a44fad
Devon Gates <devonjgates@gmail.com>
2021-04-17 21:48:48 -0500

    Created initial UI :sparkles:

--- todo/src/Todo.jsx
+++ todo/src/Todo.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import { useState, useEffect, useReducer } from 'react'
+import Alert from 'react-bootstrap/Alert'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Modal from 'react-bootstrap/Modal'
 
 const Todo = () => {
 	return (
-		<div>
-			<h1>Todo</h1>
+		<div className='container'>
+			<Jumbotron>
+				<h1>Todo</h1>
+				<p>Accomplish tasks.</p>
+			</Jumbotron>
+			<Form>
+				<Form.Group controlId='task'>
+					<Form.Label>Task</Form.Label>
+					<Form.Control type='text'></Form.Control>
+				</Form.Group>
+			</Form>
 		</div>
 	)
 }
